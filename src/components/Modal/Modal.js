aec023a3daf5aa22fe9317e0f25df034a65600d3
ines chebil <ines.chebil26@gmail.com>
2020-06-10 07:38:44 +0100

    react-transition-group-events

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,11 +8,18 @@ const animationTiming = {
 };
 const modal = (props) => {
   return (
+    //   Transitions events are helpful for staggered animations for example
     <Transition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
+      onEnter={() => console.log("onEnter")}
+      onEntering={() => console.log("onEntering")}
+      onEntered={() => console.log("onEntered")}
+      onExit={() => console.log("onExit")}
+      onExiting={() => console.log("onExiting")}
+      onExited={() => console.log("onExited")}
     >
       {(state) => {
         const cssClasses = [
